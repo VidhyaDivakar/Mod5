a421667dde12964f610341d64d78257f2a158162
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-03-29 07:15:51 -0400

    Adding error constant variables

--- interactive-registration-form/script.js
+++ interactive-registration-form/script.js
@@ -1,8 +1,28 @@
+// form elements 
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmPassword");
 const submit = document.getElementById("submit");
+
+// to display error messages, creating constant variables for the elements
+const usernameError = document.getElementById("usernameError");
+const emailError = document.getElementById("emailError");
+const passwordError = document.getElementById("passwordError");
+const confirmPasswordError = document.getElementById("confirmPasswordError");
+
+// Function to Auto-load data on page refresh
+window.addEventListener("DOMContentLoaded", handleLoad); 
+
+function handleLoad() {
+  const storedData = JSON.parse(localStorage.getItem("userData")); // user data is parsed to JS object
+
+  if (storedData) {
+    username.value = storedData.username || "";
+    email.value = storedData.email || "";
+    password.value = storedData.password || "";
+  }
+}
 
 //add event listeners
 username.addEventListener("input", ()=>{
@@ -20,7 +40,11 @@ confirmPassword.addEventListener("input", ()=>{
 // check password match add custom error message in span
 });
 
-submit.addEventListener("click", ()=>{
+submit.addEventListener("click", handleSubmit(){
+    if(!nameInput.validity.valid){
+        alert('Please enter your name.');
+        return;
+    }
 // event.preventDefault()
 //alert() save passwrd to localStroage
 //optional reset the form
@@ -42,18 +66,7 @@ function handleSave(){
     };
     localStorage.setItem("userData", JSON.stringify(userData)); // the user data is stored as string
 }
-// Auto-load on page refresh
-window.addEventListener("DOMContentLoaded", handleLoad); 
 
-function handleLoad() {
-  const storedData = JSON.parse(localStorage.getItem("userData")); // user data is parsed to JS object
-
-  if (storedData) {
-    username.value = storedData.username || "";
-    email.value = storedData.email || "";
-    password.value = storedData.password || "";
-  }
-}
 
 
 
