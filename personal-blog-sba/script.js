3f771fb04a291530543732ab2c0ca8d1b3cbeb24
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-03-30 07:00:32 -0400

    declared const variables for input elements, error messages, event listeners

--- personal-blog-sba/script.js
+++ personal-blog-sba/script.js
@@ -1,1 +1,23 @@
 console.log("This blog site is connected");
+
+// add const variable for the input, submit fields/button, errors
+
+const title = document.getElementById("title");
+const content = document.getElementById("content");
+const addPost = document.getElementById("addPost");
+
+const titleError = document.getElementById("titleError");
+const contentError = document.getElementById("contentError");
+
+// adding event listenrs for the input elements
+title.addEventListener("input", handleAdd() ){
+
+}
+
+content.addEventListener("input", handleTextContent){
+
+}
+
+addPost.addEventListener("submit", handleAddpost){
+    
+}
